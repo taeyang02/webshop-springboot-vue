1a7246442c08a9f683fde11200219ab45ca22d8e
taeyang02 <duongdongthai.smk@gmail.com>
2023-05-21 23:34:35 +0700

    update for deploy

--- FE_Webshop/src/api/baseAPI/api_call_be.js
+++ FE_Webshop/src/api/baseAPI/api_call_be.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://ec2-54-179-107-175.ap-southeast-1.compute.amazonaws.com:8080/',
+    baseURL: 'https://www.duonghasagi.online/',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
