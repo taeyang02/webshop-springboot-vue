dd95b89c215fb77e7341a8f3eca9b2304a5ab5ed
taeyang02 <duongdongthai.smk@gmail.com>
2023-05-21 23:34:35 +0700

    update for deploy

--- BE_Webshop/src/main/resources/Database/database.sql
+++ BE_Webshop/src/main/resources/Database/database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `shoptaeduong` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `shoptaeduong`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: shoptaeduong
@@ -255,4 +257,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-08 18:00:15
+-- Dump completed on 2023-05-21 21:26:27
