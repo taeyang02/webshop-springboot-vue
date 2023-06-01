94a24b0b7cefb98a5d27c9b023d3058e1370212e
Dương Đồng Thái <116984414+taeyang02@users.noreply.github.com>
2023-06-01 13:42:59 +0700

    Update menu.vue

--- FE_Webshop/src/components/commom/menu.vue
+++ FE_Webshop/src/components/commom/menu.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">TaeShop</a>
+      <a class="navbar-brand" href="#">TaeShops</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
